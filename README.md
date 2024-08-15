cfd0b370c613b2088c29903598464ebf5149b644
abidh186 <abidhussain@pursuit.org>
2024-08-15 12:39:48 -0400

    feat: add cli command that takes a file + [optional] flag and returns files stats based on flad  - setup a command line command ccwc that takes a file and an optional flag  - flags include -w, -l, -m, -c and return words, lines, bytes and chars respectively  - no flas returns bytes, lines and words by default  - add a sample.txt file to be able to perform the command line actions based on ccwc command

--- README.md
+++ README.md
@@ -16,10 +16,10 @@ I am building a command line tool that works like the Unix `wc` command. What do
 2. distinguish file types(.txt,.pdf,etc)
 3. how to tell what a "line" is
 4. how to create a terminal command on my local machine
-   1. how to add optional flags to a terminal command
+    1. how to add optional flags to a terminal command
 5. what is a byte?
-6. How to count bytes
-7. how separate a file into words
+6. how to count bytes
+7. how to separate a file into words
 
 ## Where to find stuff
 
@@ -29,19 +29,19 @@ I am building a command line tool that works like the Unix `wc` command. What do
 2. `mkdir bin`
 3. `open ~/.zshrc` (if you are using zsh as your terminal)
 4. paste `export PATH="$HOME/bin:$PATH"` onto a new line
-   1. That will tell your terminal that this folder has information it should know about
+    1. That will tell your terminal that this folder has information it should know about
 5. In your project folder
-   1. `touch index.js`
-   2. `code index.js`
-   3. In that `index.js` file
-      1. `console.log("this is my ccwc")`
+    1. `touch index.js`
+    2. `code index.js`
+    3. In that `index.js` file
+        1. `console.log("this is my ccwc")`
 6. `cd ~/bin`
 7. `touch ccwc`
 8. In that `ccwc` file
-   1. `node ~/<path to your index.js file> "$@"`
+    1. `node ~/<path to your index.js file> "$@"`
 9. In your terminal
-   1. `chmod +x ccwc`
-   2. This turns your file into an executable terminal command
+    1. `chmod +x ccwc`
+    2. This turns your file into an executable terminal command
 10. open a new terminal window
 11. `ccwc`
 12. You should see "This is my ccwc" in your terminal
--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-console.log("this is my ccwc")
+const fs = require('node:fs');
+const args = process.argv.slice(2);
+
+const isValidFileAndPath = (path) => {
+    return path.endsWith('.txt');
+};
+
+const flags = {
+    '-l': 'countLines',
+    '-w': 'countWords',
+    '-m': 'countChars',
+    '-c': 'countBytes',
+};
+
+const performAction = (action) => {
+    const path = action === 'default-action' ? args[0] : args[1];
+    console.log({ path });
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        switch (action) {
+            case 'countLines':
+                console.log(data.toString().split('\n').length + ' lines');
+                break;
+            case 'countWords':
+                console.log(data.toString().match(/\w+/g).length + ' words');
+                break;
+            case 'countChars':
+                console.log(data.toString().match(/\S/g).length + ' character');
+                break;
+            case 'countBytes':
+                console.log(fs.statSync(path).size + ' bytes');
+                break;
+            default:
+                console.log(fs.statSync(path).size + ' bytes');
+                console.log(data.toString().split('\n').length + ' lines');
+                console.log(data.toString().match(/\S/g).length + ' character');
+        }
+    });
+};
+
+if (!args.length) {
+    console.log('Error: No argument detected. Expecting two args [flag][file]');
+} else if (args.length > 2) {
+    console.log('Error: Expectin two args MAXIMUM [flag][.txt file]');
+} else if (args.length === 1) {
+    if (isValidFileAndPath(args[0])) {
+        performAction('default-action');
+    } else {
+        if (flags[args[0]]) {
+            console.log(
+                `Error: Expecting a .txt file argument with ${args[0]} flag`
+            );
+        } else {
+            console.log(`Error: ${args[0]} argument not recognized`);
+        }
+    }
+} else {
+    if (!flags[args[0]]) {
+        console.log(`Error: ${args[0]} argument not recognized`);
+    } else if (flags[args[0]] && isValidFileAndPath(args[1])) {
+        performAction(flags[args[0]]);
+    }
+}
